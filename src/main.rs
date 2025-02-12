80f0bbadad6ce02b7999998bc3540b179b742f7c
Tom <josephdotson@belkonar.com>
2025-02-11 20:38:56 -0500

    more tests

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,71 @@ fn main() {}
 
 #[cfg(test)]
 mod test {
-    fn call_no_copy(s: String) -> usize {
-        let raw_ptr: *const String = &s;
-
-        raw_ptr as usize
+    #[allow(dead_code)]
+    struct Data<'a> {
+        msg: &'a str,
     }
 
-    #[test]
-    fn no_copy() {
-        let basic_str: String = "hi".to_string();
-        let pt: *const String = &basic_str;
-        let loc = pt as usize;
-
-        let loc2 = call_no_copy(basic_str);
-        assert_eq!(loc, loc2);
+    #[allow(dead_code)]
+    struct StringData {
+        msg: String,
     }
 
-    fn call_copy<T: Copy>(s: T) -> usize {
+    fn call<T>(s: T) -> usize {
         let raw_ptr: *const T = &s;
 
         raw_ptr as usize
     }
 
     #[test]
-    fn copy() {
-        let basic_i: i32 = 42;
-        let pt: *const i32 = &basic_i;
+    fn no_copy() {
+        let data: String = "hi".to_string();
+        let pt: *const String = &data;
         let loc = pt as usize;
 
-        let loc2 = call_copy(basic_i);
+        let loc2 = call(data);
+        assert_eq!(loc, loc2);
+    }
+
+    #[test]
+    fn no_copy_struct() {
+        let data = StringData {
+            msg: "hi".to_string(),
+        };
+        let pt: *const StringData = &data;
+        let loc = pt as usize;
+
+        let loc2 = call(data);
+        assert_eq!(loc, loc2);
+    }
+
+    #[test]
+    fn copy_str() {
+        let data: &str = "hi";
+        let pt: *const &str = &data;
+        let loc = pt as usize;
+
+        let loc2 = call(data);
+        assert_ne!(loc, loc2);
+    }
+
+    #[test]
+    fn copy_int() {
+        let data: i32 = 42;
+        let pt: *const i32 = &data;
+        let loc = pt as usize;
+
+        let loc2 = call(data);
+        assert_ne!(loc, loc2);
+    }
+
+    #[test]
+    fn copy_struct() {
+        let data: Data = Data { msg: "hi" };
+        let pt: *const Data = &data;
+        let loc = pt as usize;
+
+        let loc2 = call(data);
         assert_ne!(loc, loc2);
     }
 }
